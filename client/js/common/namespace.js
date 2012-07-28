9d596752dc1137e8db59de7858420a73ae048091
Carlos Zuniga <carlosluiszuniga@gmail.com>
2012-07-28 16:28:21 -0700

    Added parse lib

--- client/js/common/namespace.js
+++ client/js/common/namespace.js
@@ -2,3 +2,4 @@ window.co2 = window.co2 || { };
 window.co2.view = window.co2.view || { };
 window.co2.colection = window.co2.collection || { };
 window.co2.object = window.co2.collection || { };
+Parse.initialize("glA2qVYleTleiRWsqapf3gdtLvH7ckbsDue1WmjX", "iJ5l8ftgcRfObNjQzXNhdP4tlnEOC2zCHWqvryjq");
