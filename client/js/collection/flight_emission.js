a0ceabac5c65a553c6800d66868529682e4f91b2
Ross Solomon <rsolomon@climate.com>
2012-07-28 20:13:33 -0700

    Get flight emissions

--- client/js/collection/flight_emission.js
+++ client/js/collection/flight_emission.js
@@ -1,10 +1,21 @@
-window.co2.collection.FlightEmissionCollection = window.co2.collection.AMEECollection.extend({
+co2.collection.FlightEmissionCollection = co2.collection.AMEECollection.extend({
   data: {
-    'path': '/3.6/categories/Great_Circle_flight_methodology/calculation',
+    'path': '/3.6/categories/Great_Circle_flight_methodology/calculation'
 
-    //TEMP
-    'values.IATAcode1': 'LHR',
-    'values.IATAcode2': 'LAX'
+//    //TEMP
+//    'values.IATAcode1': 'LHR',
+//    'values.IATAcode2': 'LAX'
+  },
+
+  fetch: function(options) {
+    var data = {
+      'values.IATAcode1': options.data.departing,
+      'values.IATAcode2': options.data.arriving
+    };
+
+    co2.collection.AMEECollection.prototype.fetch.call(this, {
+      data: data
+    });
   },
 
   parse: function(data) {
--- client/js/view/add_flight.js
+++ client/js/view/add_flight.js
@@ -1,6 +1,25 @@
 window.co2.view.AddFlightView = Backbone.View.extend({
   _addFlightFormTemplate: '#template-add-flight',
 
+  events: {
+    'click button': 'onSubmitClick'
+  },
+
+  onSubmitClick: function() {
+    var arriving = this.$('.flight-arriving').data('selected'),
+      departing = this.$('.flight-departing').data('selected');
+
+    this.collection.fetch({
+      data: {
+        arriving: arriving,
+        departing: departing
+      }
+    });
+//    _.each(this.$('input'), function(input) {
+//      alert($(input).data('selected'));
+//    });
+  },
+
   initialize: function() {
 
     // Initialize templates
@@ -14,14 +33,24 @@ window.co2.view.AddFlightView = Backbone.View.extend({
 
     var options = {
       serviceUrl: 'http://localhost:1337/flights',
-      width: 384,
+      width: 250,
       delimiter: /(,|;)\s*/,
       deferRequestBy: 10, //miliseconds
-      noCache: true, //set to true, to disable caching
-      onSelect: function(name, key) {
-        $(this).data('selected', key);
-      }
+      noCache: true
     };
-    this.$('input').autocomplete(options);
+
+    var $arriving = this.$('.flight-arriving'),
+      $departing = this.$('.flight-departing');
+
+    $arriving.autocomplete(_.extend({
+      onSelect: function(value, key) {
+        $arriving.data('selected', key);
+      }
+    }, options));
+    $departing.autocomplete(_.extend({
+      onSelect: function(value, key) {
+        $departing.data('selected', key);
+      }
+    }, options));
   }
 });
